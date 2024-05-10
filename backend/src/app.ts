0e47a01252581fe3c69fb4e143fe426b35d38c26
Abbas Hassan <150517260+Slayer-Regen@users.noreply.github.com>
2024-05-10 20:25:31 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://deploy-mern-ai-bot-12.vercel.app",methods: ["POST" , "GET"] , credentials: true }));
+app.use(cors({ origin: "http://mern-ai-bot-12.vercel.app",methods: ["POST" , "GET"] , credentials: true }));
 app.use(express.json());
 mongoose.connect('mongodb+srv://slayer:NlCrSaq4GhH2TSPx@cluster0.qh1zo6f.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0');
 app.use(cookieParser(process.env.COOKIE_SECRET));
